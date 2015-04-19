054fefb967f9e591608d86f73e509b6cf89bdb49
Ching Yi, Chan <chingyichan.tw@gmail.com>
2015-04-20 01:26:47 +0800

    cache something

---
+++ hadoop-itemcount/mapreduce/src/main/java/org/phstudy/EHCWebLogsCachedPidPricedMapper.java
@@ -0,0 +1,63 @@
+package org.phstudy;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class EHCWebLogsCachedPidPricedMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+
+    public final static String EXPECTED_ACT = ";act=order;";
+    public final static String ORDER_LIST_START = "plist=";
+    public final static String ORDER_LIST_END = ";";
+    public final static String EMPTY_ORDER_LIST = ";plist=;";
+
+    private static HashMap<String, Integer> pidAndPriceMap = new HashMap<String, Integer>();
+    private static HashMap<String, Text> pidTextMap = new HashMap<String, Text>();
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        String input = value.toString();
+
+        if (!input.contains(EXPECTED_ACT) || input.contains(EMPTY_ORDER_LIST)) {
+            return;
+        }
+
+        int pStart = input.indexOf(ORDER_LIST_START);
+        if (pStart != -1) {
+            pStart += ORDER_LIST_START.length();
+        }
+        int eStart = input.indexOf(ORDER_LIST_END, pStart);
+
+        String originData = input.substring(pStart, eStart);
+        String[] data = originData.split(",");
+        if (data.length % 3 != 0) {
+            System.err.println("ERR " + input);
+        }
+
+        for (int x = 1; x < data.length; x += 3) {
+            try {
+
+                String pid = data[x - 1];
+                if (pidAndPriceMap.containsKey(pid)) {
+                    context.write(pidTextMap.get(pid),
+                            new LongWritable(Integer.parseInt(data[x]) * pidAndPriceMap.get(pid)));
+                } else {
+                    Integer price = Integer.parseInt(data[x + 1]);
+                    context.write(new Text(pid), new LongWritable(Integer.parseInt(data[x]) * price));
+                    pidAndPriceMap.put(pid, price);
+                    pidTextMap.put(pid, new Text(pid));
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+}
