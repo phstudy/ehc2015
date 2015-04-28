8c5d38a663565692c615028a702ab13599550e34
Study <ph.study@gmail.com>
2015-04-28 18:39:52 +0800

    remove redundant bytes

--- hadoop-itemcount/mapreduce/src/main/java/org/phstudy/job/CopyFileDirect.java
+++ hadoop-itemcount/mapreduce/src/main/java/org/phstudy/job/CopyFileDirect.java
@@ -1,5 +1,6 @@
 package org.phstudy.job;
 
+import com.google.common.primitives.Bytes;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
@@ -38,8 +39,12 @@ public class CopyFileDirect implements Runnable {
             fsdos.write(outbuf, 512, len - 512); // skip tar header
 
             while ((len = zgis.read(outbuf, 0, 65536)) != -1) {
+                if (outbuf[len - 1] == 0x00) {
+                    break;
+                }
                 fsdos.write(outbuf, 0, len);
             }
+            fsdos.write(outbuf, 0, Bytes.indexOf(outbuf, (byte) 0x00));
 
             zgis.close();
             fsdos.close();
